a458f3d660fe9603988a9c6b628923d7d4d13259
PizzieT <167532437+PizzieT@users.noreply.github.com>
2026-07-28 06:32:35 -0500

    fix: remove unsupported iOS transition override

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -40,7 +40,6 @@ class AppTheme {
       pageTransitionsTheme: const PageTransitionsTheme(
         builders: {
           TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
-          TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
         },
       ),
       appBarTheme: const AppBarTheme(
